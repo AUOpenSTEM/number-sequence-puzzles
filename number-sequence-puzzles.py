3a2291bfe1d44958aca6215437dabd8d2b77e4bf
Arjen Lentz <arjenlentz@catalyst-au.net>
2020-02-01 17:02:56 +1000

    initial commit

---
+++ number-sequence-puzzles.py
@@ -0,0 +1,14 @@
+# number sequence puzzle generator
+# Copyright (C) 2020 by Arjen Lentz
+# Licensed under AGPLv3
+
+# 2020-02-01 initial stuffs
+
+import random
+
+
+random.seed()
+
+for i in range (0,10):
+    x = random.randint(1,100)
+    print(x)
